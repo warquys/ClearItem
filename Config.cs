c1897023acfef41515f6c1b4e33ec027df471957
warquys <64769541+warquys@users.noreply.github.com>
2022-11-13 21:04:18 +0100

    Ajoutez des fichiers projet.

---
+++ Config.cs
@@ -0,0 +1,12 @@
+﻿using Synapse.Config;
+using System.ComponentModel;
+
+namespace ClearItem
+{
+    public class Config : AbstractConfigSection
+    {
+        [Description("The time before an item on the ground despawns in seconde")]
+        public int TimeBeforeDespawn { get; set; } = 300;
+        
+    }
+}
