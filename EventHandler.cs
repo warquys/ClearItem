ed1493f0bc884610786fc7f6a0ebe46776e97c00
warquys <64769541+warquys@users.noreply.github.com>
2022-11-13 21:04:18 +0100

    remove the debug log

---
+++ EventHandler.cs
@@ -0,0 +1,68 @@
+﻿using MEC;
+using Synapse.Api;
+using Synapse.Api.Enum;
+using Synapse.Api.Events.SynapseEventArguments;
+using Synapse.Api.Items;
+using System.Collections.Generic;
+
+namespace ClearItem
+{
+    public class EventHandler
+    {
+
+        #region Properties & Variables
+        Dictionary<SynapseItem, CoroutineHandle> ItemsCoroutine = new Dictionary<SynapseItem, CoroutineHandle>();
+        #endregion
+
+        #region Constructor & Destructor
+        public EventHandler()
+        {
+            Synapse.Api.Events.EventHandler.Get.Player.PlayerDropItemEvent += OnDropItem;
+            Synapse.Api.Events.EventHandler.Get.Player.PlayerPickUpItemEvent += OnPickup;
+        }
+        #endregion
+
+        #region Methods
+        private void CoroutineStartFor(SynapseItem item)
+        {
+            CoroutineStopFor(item);
+            CoroutineHandle coroutine = Timing.RunCoroutine(DeleteAfterTime(item));
+            ItemsCoroutine.Add(item, coroutine);
+        }
+        private bool CoroutineStopFor(SynapseItem item)
+        {
+            if (ItemsCoroutine.ContainsKey(item))
+            {
+                Timing.KillCoroutines(ItemsCoroutine[item]);
+                return ItemsCoroutine.Remove(item);
+            }
+            return false;
+        }
+
+        private IEnumerator<float> DeleteAfterTime(SynapseItem item)
+        {
+            yield return Timing.WaitForSeconds(Plugin.Instance.Config.TimeBeforeDespawn);
+
+            if (item is null) yield break;
+
+            if (item.State == ItemState.Map)
+                item.Despawn();
+
+
+        }
+        #endregion
+
+        #region Events
+
+        private void OnDropItem(PlayerDropItemEventArgs ev)
+        {
+            CoroutineStartFor(ev.Item);
+        }
+
+        private void OnPickup(PlayerPickUpItemEventArgs ev)
+        {
+            CoroutineStopFor(ev.Item);
+        }
+        #endregion
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using Synapse.Api.Plugin;
+
+namespace ClearItem
+{
+    [Synapse.Api.Plugin.PluginInformation(
+           Author = "VT",
+           Description = "Remove items from the floor",
+           Name = "ClearItem",
+           SynapseMajor = SynapseController.SynapseMajor,
+           SynapseMinor = SynapseController.SynapseMinor,
+           SynapsePatch = SynapseController.SynapsePatch,
+           Version = "v.1.0.0"
+           )]
+    public class Plugin : AbstractPlugin
+    {
+        public static Plugin Instance { get; set; }
+
+        [Synapse.Api.Plugin.Config(section = "ClearItem")]
+        public Config Config { get; set; }
+
+        public EventHandler EventHandler { get; set;}
+
+        public override void Load()
+        {
+            new EventHandler();
+            Instance = this;
+
+
+        }
+
+    }
+}
